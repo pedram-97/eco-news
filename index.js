5de1cbe53f26155c16e009ad3f122d258da01bd2
pedram-azizi <pedramazizi96@gmail.com>
2024-03-11 11:22:48 +0330

    Fix appearance issues-footer/sections-hover/spacing

---
+++ index.js
@@ -0,0 +1,8 @@
+var dropDownContent = document.getElementById("newDropDown");
+dropDownContent.style.display = "none";
+function mouseOverToggle() {
+  dropDownContent.style.display = "";
+}
+function mouseOutToggle() {
+  dropDownContent.style.display = "none";
+}
